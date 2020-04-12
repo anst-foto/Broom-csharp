47e89a5f92799ac04654375491242c0753eae4ab
anst <starininandrey@gmail.com>
2020-04-12 20:48:20 +0300

    v0.10
    v0.10 (Апрель 2020):  Добавление GUI

---
+++ BroomGUI/BroomLogFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace BroomGUI
+{
+    public static class BroomLogFile
+    {
+        private const string logFilePath = @"broom.log";
+
+        private static void WriteLogFile(string message)
+        {
+            try
+            {
+                using var logFile = new StreamWriter(logFilePath, true, Encoding.Default);
+                logFile.WriteLine(message);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        public static void LogFileStart()
+        {
+            var msg = "=============== " + DateTime.Today.ToString() + " ===============";
+            WriteLogFile(msg);
+        }
+        public static void LogFileEnd()
+        {
+            const string msg = "=============== END ===============";
+            WriteLogFile(msg);
+        }
+        public static void SuccessfullyMessage(string message)
+        {
+            var msg = DateTime.Now.ToString();
+            msg += " - Successfully : ";
+            msg += message;
+            WriteLogFile(msg);
+        }
+        public static void ErrorMessage(string message)
+        {
+            var msg = DateTime.Now.ToString();
+            msg += " - Error : ";
+            msg += message;
+            WriteLogFile(msg);
+        }
+        public static void InfoMessage(string message)
+        {
+            var msg = DateTime.Now.ToString();
+            msg += " - Info : ";
+            msg += message;
+            WriteLogFile(msg);
+        }
+    }
+}
---
+++ BroomGUI/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace BroomGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            #region Event
+            Broom.Info += InfoMessage;
+            Broom.Error += ErrorMessage;
+            Broom.Successfully += SuccessfullyMessage;
+
+            Broom.Info += BroomLogFile.InfoMessage;
+            Broom.Error += BroomLogFile.ErrorMessage;
+            Broom.Successfully += BroomLogFile.SuccessfullyMessage;
+            #endregion
+        }
+        
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioCleanerBrowser.IsChecked == true)
+            {
+                Broom.CleanerBrowser();
+            }
+            else if (radioCleanerDownloads.IsChecked == true)
+            {
+                Broom.CleanerDownloads();
+            }
+            else if (radioCleanerRecile.IsChecked == true)
+            {
+                Broom.CleanerRecile();
+            }
+            else if (radioCleanerBrowserAndDownloads.IsChecked == true)
+            {
+                Broom.CleanerBrowser();
+                Broom.CleanerDownloads();
+            }
+            else if (radioCleanerBrowserAndRecile.IsChecked == true)
+            {
+                Broom.CleanerBrowser();
+                Broom.CleanerRecile();
+            }
+            else if (radioCleanerRecileAndDownloads.IsChecked == true)
+            {
+                Broom.CleanerRecile();
+                Broom.CleanerDownloads();
+            }
+            else if (radioCleanerAll.IsChecked == true)
+            {
+                Broom.CleanerAll();
+            }
+        }
+
+        #region Show
+        private void InfoMessage(string message)
+        {
+            Log.Foreground = Brushes.Yellow;
+            Log.Inlines.Add("----------\n");
+            var msg = "Info: " + message + "\n";
+            Log.Inlines.Add(msg);
+            Log.Inlines.Add("----------\n");
+            Log.Foreground = Brushes.Black;
+        }
+
+        private void ErrorMessage(string message)
+        {
+            Log.Foreground = Brushes.Red;
+            Log.Inlines.Add("!!!!!!!!!!\n");
+            var msg = "Error: " + message + "\n";
+            Log.Inlines.Add(msg);
+            Log.Inlines.Add("!!!!!!!!!!\n");
+            Log.Foreground = Brushes.Black;
+        }
+
+        private void SuccessfullyMessage(string message)
+        {
+            Log.Foreground = Brushes.DarkGreen;
+            Log.Inlines.Add("**********\n");
+            var msg = "Successfully: " + message + "\n";
+            Log.Inlines.Add(msg);
+            Log.Inlines.Add("**********\n");
+            Log.Foreground = Brushes.Black;
+        }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BroomLogFile.LogFileStart();
+        }
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            BroomLogFile.LogFileEnd();
+        }
+    }
+}
