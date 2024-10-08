e64c86674bad5c95b8ac6e656f6430efe553cb40
Starinin Andrey <starininandrey@gmail.com>
2024-10-08 19:42:45 +0300

    Добавление Core.Test

---
+++ Broom.Core.Test/DeleteServiceTest.cs
@@ -0,0 +1,14 @@
+namespace Broom.Core.Test;
+
+public class DeleteServiceTest
+{
+    [Fact]
+    public void RemovingReadOnlyAttributeFromFiles_PositiveTest()
+    {
+    }
+
+    [Fact]
+    public void RemovingReadOnlyAttributeFromFiles_NegativeTest()
+    {
+    }
+}
