38b36cc7fe13b080ef01975f4e9ae9047de88325
Starinin Andrey <starininandrey@gmail.com>
2024-10-08 18:57:32 +0300

    Создание класса для удаления файлов из папки

---
+++ Broom.Core/DeleteService.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using NLog;
+
+namespace Broom.Core;
+
+public static class DeleteService
+{
+    private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+    public static void RemovingReadOnlyAttributeFromFiles(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Logger.Error($"Directory {path} does not exist");
+            return;
+        }
+
+        var directory = new DirectoryInfo(path);
+        directory.Attributes &= ~FileAttributes.ReadOnly;
+        Logger.Info($"Set {directory.FullName} attributes to normal");
+
+        var files = directory.GetFiles();
+        foreach (var file in files)
+        {
+            file.Attributes = FileAttributes.Normal;
+            Logger.Info($"Set {file.FullName} attributes to normal");
+        }
+    }
+
+    public static void DeleteFiles(string path)
+    {
+        var directory = new DirectoryInfo(path);
+        var files = directory.GetFiles();
+        foreach (var file in files)
+        {
+            file.Delete();
+            Logger.Info($"Delete {file.FullName}");
+        }
+
+        directory.Delete(recursive: true);//FIXME  ??? recursive ???
+        Logger.Info($"Delete {directory.FullName}");
+    }
+}
