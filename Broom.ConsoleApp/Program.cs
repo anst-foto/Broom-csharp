f18570d065924affe4c36a054b5ab5f8c41ddf14
Starinin Andrey <starininandrey@gmail.com>
2024-10-08 21:04:01 +0300

    Добавление приветствия и обработки выбора режима пользователем

--- Broom.ConsoleApp/Program.cs
+++ Broom.ConsoleApp/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Broom.ConsoleApp;
+using Broom.Core;
 
-Console.WriteLine("Hello, World!");
+ConsoleHelper.PrintWelcome();
+ConsoleHelper.PrintMenu();
+
+var select = Console.ReadLine();
+
+switch (select)
+{
+    case "1": // 1. Очистить только кэши браузеров
+
+        break;
+    case "2": // 2. Очитстить только Корзину и временные файлы (RecycleBin & Temp)
+        Cleaner.CleaningRecycleBinWinApi();
+        Cleaner.CleaningTemp();
+        break;
+
+    case "3": // 3. Очитстить только папку Загрузки (Downloads)
+
+        break;
+
+    case "4": // 4. Очитстить кэши браузеров и Корзину с временными файлами (RecycleBin & Temp)
+
+        break;
+
+    case "5": // 5. Очитстить кэши браузеров и папку Загрузки (Downloads)
+
+        break;
+
+    case "6": // 6. Очитстить Корзину с временными файлами (RecycleBin & Temp) и папку Загрузки (Downloads)
+
+        break;
+
+    case "7": // 7. Очитстить кэши браузеров, Корзину с временными файлами (RecycleBin & Temp) и папку Загрузки (Downloads)
+
+        break;
+
+    case "0": // 0. Выход
+        break;
+
+    default:
+        Console.WriteLine("Неверный режим работы");
+        break;
+}
+
+Console.ReadLine();
