e0eeb6bc81487ac34064210258cb823ba73bc700
Starinin Andrey <starininandrey@gmail.com>
2024-10-09 12:13:31 +0300

    Разделение класса ConsoleHelper

---
+++ Broom.ConsoleApp/ConsoleHelper/ConsoleHelper.ShowMessage.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+
+namespace Broom.ConsoleApp.ConsoleHelper;
+
+public static partial class ConsoleHelper
+{
+    /// <summary>
+    /// Вывод информационного сообщения в консоль
+    /// </summary>
+    /// <param name="message">Текст сообщения</param>
+    public static void InfoMessage(string message)
+    {
+        ForegroundColor = ConsoleColor.Yellow;
+        WriteLine();
+        WriteLine("----------");
+        WriteLine("Info");
+        WriteLine(message);
+        WriteLine("----------");
+        WriteLine();
+
+        ResetColor();
+    }
+
+    /// <summary>
+    /// Вывод сообщения об ошибке в консоль
+    /// </summary>
+    /// <param name="message">Текст сообщения</param>
+    public static void ErrorMessage(string message)
+    {
+        ForegroundColor = ConsoleColor.Red;
+        WriteLine();
+        WriteLine("!!!!!!!!!!");
+        WriteLine("ERROR");
+        WriteLine(message);
+        WriteLine("!!!!!!!!!!");
+        WriteLine();
+
+        ResetColor();
+    }
+
+    /// <summary>
+    /// Вывод сообщения об успешном завершении в консоль
+    /// </summary>
+    /// <param name="message">Текст сообщения</param>
+    public static void SuccessfullyMessage(string message)
+    {
+        ForegroundColor = ConsoleColor.DarkGreen;
+        WriteLine();
+        WriteLine("**********");
+        WriteLine("Successfully");
+        WriteLine(message);
+        WriteLine("**********");
+        WriteLine();
+
+        ResetColor();
+    }
+}
--- Broom.ConsoleApp/ConsoleHelper/ConsoleHelper.cs
+++ Broom.ConsoleApp/ConsoleHelper/ConsoleHelper.cs
@@ -1,11 +1,11 @@
 ﻿using static System.Console;
 
-namespace Broom.ConsoleApp;
+namespace Broom.ConsoleApp.ConsoleHelper;
 
 /// <summary>
 /// Класс для вывода информации в консоль
 /// </summary>
-public static class ConsoleHelper
+public static partial class ConsoleHelper
 {
     /// <summary>
     /// Вывод приветствия в консоль
@@ -90,57 +90,6 @@ public static class ConsoleHelper
         WriteLine(
             "7. Очитстить кэши браузеров, Корзину с временными файлами (RecycleBin & Temp) и папку Загрузки (Downloads)");
         WriteLine("0. Выход");
-        WriteLine();
-
-        ResetColor();
-    }
-
-    /// <summary>
-    /// Вывод информационного сообщения в консоль
-    /// </summary>
-    /// <param name="message">Текст сообщения</param>
-    public static void InfoMessage(string message)
-    {
-        ForegroundColor = ConsoleColor.Yellow;
-        WriteLine();
-        WriteLine("----------");
-        WriteLine("Info");
-        WriteLine(message);
-        WriteLine("----------");
-        WriteLine();
-
-        ResetColor();
-    }
-
-    /// <summary>
-    /// Вывод сообщения об ошибке в консоль
-    /// </summary>
-    /// <param name="message">Текст сообщения</param>
-    public static void ErrorMessage(string message)
-    {
-        ForegroundColor = ConsoleColor.Red;
-        WriteLine();
-        WriteLine("!!!!!!!!!!");
-        WriteLine("ERROR");
-        WriteLine(message);
-        WriteLine("!!!!!!!!!!");
-        WriteLine();
-
-        ResetColor();
-    }
-
-    /// <summary>
-    /// Вывод сообщения об успешном завершении в консоль
-    /// </summary>
-    /// <param name="message">Текст сообщения</param>
-    public static void SuccessfullyMessage(string message)
-    {
-        ForegroundColor = ConsoleColor.DarkGreen;
-        WriteLine();
-        WriteLine("**********");
-        WriteLine("Successfully");
-        WriteLine(message);
-        WriteLine("**********");
         WriteLine();
 
         ResetColor();
