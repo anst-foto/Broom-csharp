c697c572d01a3adb3f1979d9439aba00b05fc60a
Starinin Andrey <starininandrey@gmail.com>
2024-10-09 13:56:34 +0300

    Добавление новых исключений

--- Broom.Core/Exceptions/Exception.cs
+++ Broom.Core/Exceptions/Exception.cs
@@ -7,7 +7,7 @@ namespace Broom.Core.Exceptions;
 /// </summary>
 /// <param name="path">Путь к файлу/папке</param>
 /// <param name="innerException">Вложенное исключение</param>
-public class ReadOnlyAttributeException(string path, Exception innerException = null)
+public class ReadOnlyAttributeException(string path, Exception? innerException = null)
     : Exception($"Не удалось установить режим доступа у {path}", innerException);
 
 /// <summary>
@@ -15,7 +15,7 @@ public class ReadOnlyAttributeException(string path, Exception innerException = 
 /// </summary>
 /// <param name="path">Путь к файлу/папке</param>
 /// <param name="innerException">Вложенное исключение</param>
-public class NotFoundException(string path, Exception innerException = null)
+public class NotFoundException(string path, Exception? innerException = null)
     : Exception($"Не найдено {path}", innerException);
 
 /// <summary>
@@ -23,9 +23,17 @@ public class NotFoundException(string path, Exception innerException = null)
 /// </summary>
 /// <param name="path">Путь к файлу/папке</param>
 /// <param name="innerException">Вложенное исключение</param>
-public class DeleteException(string path, Exception innerException = null)
+public class DeleteException(string path, Exception? innerException = null)
     : Exception($"Не удалось удалить {path}", innerException);
 
-
+/// <summary>
+/// Исключение, возникающее если не удалось очистить корзину.
+/// </summary>
 public class CleaningRecycleBinException()
     : Exception($"Не удалось очистить корзину");
+
+/// <summary>
+/// Исключение, возникающее если не удалось очистить временные файлы.
+/// </summary>
+public class CleaningTempException()
+    : Exception($"Не удалось очистить временные файлы");
