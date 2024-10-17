e81916f382aa50a5f3ee4223e368640fb3080e0c
Starinin Andrey <starininandrey@gmail.com>
2024-10-17 13:52:47 +0300

    Добавление меню TUI-приложения

--- Broom.TUI/MyView.cs
+++ Broom.TUI/MyView.cs
@@ -10,6 +10,41 @@ public class MyView : Window
     {
         Title = $"Broom ({Application.QuitKey} для выхода)";
 
+        var menu = new MenuBar();
+        menu.Menus =
+        [
+            new MenuBarItem(
+                title: "_Файл",
+                children: new MenuItem[]
+            {
+                new(
+                    title: "_Закрыть",
+                    help: "Выйти из программы",
+                    action: () => Application.RequestStop()),
+            }),
+
+            new MenuBarItem(
+                title: "_Помощь",
+                children: new MenuItem[]
+            {
+                new(
+                    title: "_О программе",
+                    help: string.Empty,
+                    action: () =>
+                {
+                    MessageBox.Query(
+                        title: "Broom",
+                        message: """
+                                 Очистка кэша и Корзины, удаление временных файлов
+                                 Старинин Андрей Николаевич (AnSt), 2024
+                                 GNU General Public License v3.0
+                                 Версия: 0.1 (Октябрь 2024)
+                                 """,
+                        buttons: "Закрыть");
+                }),
+            })
+        ];;
+
         var buttonClear = new Button
         {
             Text = "Очистить",
@@ -32,19 +67,28 @@ public class MyView : Window
             cleaner.Clean();
             if (cleaner.Errors.Count == 0)
             {
-                MessageBox.Query("Broom", "Очистка завершена успешно");
+                MessageBox.Query(
+                    title: "Broom",
+                    message: "Очистка завершена успешно");
             }
             else
             {
 
-                MessageBox.ErrorQuery("Broom", "Ошибки при очистке", "Ok");
+                MessageBox.ErrorQuery(
+                    title: "Broom",
+                    message: "Ошибки при очистке",
+                    buttons: "Ok");
+
                 foreach (var error in cleaner.Errors)
                 {
-                    MessageBox.ErrorQuery("Broom", error.Message, "Ok");
+                    MessageBox.ErrorQuery(
+                        title: "Broom",
+                        message: error.Message,
+                        buttons: "Ok");
                 }
             }
         };
 
-        Add(buttonClear);
+        Add(menu, buttonClear);
     }
 }
